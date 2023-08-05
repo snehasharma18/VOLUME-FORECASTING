90cfe1c93cebe218629e33b72f39970ce274bda9
Sneha sharma <104129497+snehasharma18@users.noreply.github.com>
2023-08-05 16:24:47 +0530

    Update Volume.py

--- Volume.py
+++ Volume.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 from prophet import Prophet
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -15,44 +9,20 @@ from prophet.diagnostics import cross_validation, performance_metrics
 import plotly.express as px
 import plotly.graph_objects as go
 
-
-# In[2]:
-
-
 data=pd.read_excel('Model Final Data.xlsx')
-
-
-# In[3]:
-
 
 data.rename(
     columns={"Month-Year":"ds", "Volume":"y"}, 
     inplace=True
 )
 
-
-# In[4]:
-
-
 data['Region'].value_counts()
-
-
-# In[5]:
-
 
 regions = ['AMER','APAC','EMEA']
 subprocess = ['E2C','IP']
 function = ['Accounts Payable']
 
-
-# In[6]:
-
-
 desired_rows = 6
-
-
-# In[7]:
-
 
 forecast_start_date = pd.to_datetime('2023-05-01')  # Replace with your desired start date
 forecast_end_date = forecast_start_date + pd.DateOffset(months=6)
@@ -63,15 +33,7 @@ limited_regions = (regions * (desired_rows // len(regions))) + regions[:desired_
 limited_subprocess = (subprocess * (desired_rows // len(subprocess))) + subprocess[:desired_rows % len(subprocess)]
 limited_functions = (function * (desired_rows // len(function))) + function[:desired_rows % len(function)]
 
-
-# In[8]:
-
-
 len(future_dates)
-
-
-# In[9]:
-
 
 future = pd.DataFrame({
     'ds': limited_dates,
@@ -80,116 +42,44 @@ future = pd.DataFrame({
     'subprocess': limited_subprocess
 })
 
-
-# In[10]:
-
-
 df=data.copy()
-
-
-# In[11]:
-
 
 df.info()
 
-
-# In[12]:
-
-
 df["ds"] = pd.to_datetime(df["ds"])
-
-
-# In[13]:
-
 
 df.isnull().sum()
 
-
-# In[14]:
-
-
 df.describe()
-
-
-# In[15]:
-
 
 col_to_drop = ["Core","Company","Active Status"]
 df = df.drop(col_to_drop, axis=1)
 
-
-# In[16]:
-
-
 forecast_start_date = pd.to_datetime('2023-05-01')
 
-
-# In[17]:
-
-
 forecast_end_date = forecast_start_date + pd.DateOffset(months=6)
-
-
-# In[18]:
-
 
 train_data = df[df['ds'] < forecast_start_date][['ds', 'Region', 'Sub Process','Function','y']]
 test_data = df[(df['ds'] >= forecast_start_date) & (df['ds'] <= forecast_end_date)]
 
-
-# In[19]:
-
-
 model = Prophet()
 model.fit(train_data)
 
-
-# In[20]:
-
-
 forecast = model.predict(future)
-
-
-# In[21]:
-
 
 forecast_with_region_subprocess = pd.concat([future, forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]], axis=1)
 
-
-# In[22]:
-
-
 predicted_values = forecast['yhat'][-90:]
 
-
-# In[23]:
-
-
 actual_values = df[(df['ds'] >= forecast_start_date) & (df['ds'] <= forecast_end_date)]['y']
-
-
-# In[24]:
-
 
 mae = mean_absolute_error(actual_values, predicted_values)
 print("Mean Absolute Error:", mae)
 
-
-# In[25]:
-
-
 rmse = mean_squared_error(actual_values, predicted_values, squared=False)
 print("Root Mean Squared Error:", rmse)
 
-
-# In[26]:
-
-
 st.title('Volume Forecasting App')
-
-
-# In[27]:
-
 
 region = st.selectbox('Select Region', df['Region'].unique())
 subprocess = st.selectbox('Select Subprocess', df['Sub Process'].unique())
@@ -205,10 +95,6 @@ selected_month = st.selectbox('Select Month', range(1, 13))
 forecast_start_date = pd.to_datetime(f'{selected_year}-{selected_month:02d}-01').date()
 
 predict_button = st.button("Predict")
-
-
-# In[28]:
-
 
 if predict_button:
     # Convert forecast start date to datetime64[ns]
@@ -258,10 +144,6 @@ if predict_button:
     # Display the line chart using Streamlit
     with st.container():
         st.plotly_chart(fig)
-
-
-# In[ ]:
-
 
 
 
